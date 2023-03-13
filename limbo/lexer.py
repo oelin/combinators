536e84b4dc34de2de65826d248286d23fc48861b
kale <42823429+oelin@users.noreply.github.com>
2023-03-13 13:53:35 +0000

    Update lexer.py

--- limbo/lexer.py
+++ limbo/lexer.py
@@ -3,19 +3,38 @@ from re import match
 from . parser import Parse, Parser, Success, Failure
 
 
+def string(string: str) -> Parse:
+    """
+    string(string: str) -> Parse
+
+    Converts a Python string into a parse string.
+    """
+
+    return Parse(
+            string = string,
+            result = None,
+    )
+
+
+
 class Match(Parser):
+    """
+    Match(Parser)
 
-        def __init__(self, token: str):
-                self.token = token
+    Consumes a parse string if it matches a given token.
+    """
 
-        def __call__(self, parse: Parser) -> Parse:
+    def __init__(self, token: str):
+        self.token = token
 
-                lexeme = match(self.token, parse.string)
+    def __call__(self, parse: Parser) -> Parse:
 
-                if lexeme == None:
-                        return Failure(parse)
+        lexeme = match(self.token, parse.string)
 
-                return Success(Parse(
-                        string = parse.string[lexeme.end() :],
-                        result = lexeme.group(),
-                ))
+        if lexeme == None:
+            return Failure(parse)
+
+        return Success(Parse(
+                string = parse.string[lexeme.end() :],
+                result = lexeme.group(),
+        ))
