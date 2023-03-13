ae0a4b079e2ec9e7d4f6703c8fc8e3b7a985fd7b
kale <42823429+oelin@users.noreply.github.com>
2023-03-13 18:15:51 +0000

    Update and rename lexer.py to parsers.py

---
+++ limbo/parsers.py
@@ -0,0 +1,21 @@
+"""
+Common parsers.
+"""
+
+from dataclasses import dataclass
+from re import match
+from parser import Parse, Parser
+
+
+@dataclass
+class Token(Parser):
+    token: str
+
+    def __call__(self, parse: Parser) -> Parse:
+
+        lexeme = match(self.token, parse[0])
+
+        if lexeme:
+                return (parse[0][lexeme.end() :], lexeme.group())
+
+    __repr__ = lambda self: f"\"{self.token}\""
