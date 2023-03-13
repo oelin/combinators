b15548971ad61a5c478a18582ba30584addbe240
kale <42823429+oelin@users.noreply.github.com>
2023-03-13 13:46:48 +0000

    Create lexer.py

---
+++ limbo/lexer.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from re import match
+from . parser import Parse, Parser, Success, Failure
+
+
+class Match(Parser):
+
+        def __init__(self, token: str):
+                self.token = token
+
+        def __call__(self, parse: Parser) -> Parse:
+
+                lexeme = match(self.token, parse.string)
+
+                if lexeme == None:
+                        return Failure(parse)
+
+                return Success(Parse(
+                        string = parse.string[lexeme.end() :],
+                        result = lexeme.group(),
+                ))
