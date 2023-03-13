9f71f84686be13617c26b669aae0216b6c802df2
kale <42823429+oelin@users.noreply.github.com>
2023-03-13 13:07:56 +0000

    Update __init__.py

--- limbo/__init__.py
+++ limbo/__init__.py
@@ -6,154 +6,154 @@ from dataclasses import dataclass
 
 @dataclass
 class Parse:
-	"""
-	Parse
-	
-	An abstract base class for parses.
-	"""
-	
-        string: str
-        result: Any
-        failed: bool = False
+    """
+    Parse
+
+    An abstract base class for parses.
+    """
+
+    string: str
+    result: Any
+    failed: bool = False
 
 
 class Parser:
-	"""
-	Parser
-	
-	An abstract base class for parsers (transformations on parses).
-	"""
+    """
+    Parser
 
-        def __call__(self, parse: Parse) -> Parse:
-                raise NotImplementedError()
+    An abstract base class for parsers (transformations on parses).
+    """
+
+    def __call__(self, parse: Parse) -> Parse:
+        raise NotImplementedError()
 
 
 class ParserCombinator:
-	"""
-	ParserCombinator
-	
-	An abstract base class for parser combinators (transformations
-	on parsers).
-	"""
+    """
+    ParserCombinator
 
-        def __call__(self, *parsers: Tuple[Parser]) -> Parser:
-                raise NotImplementedError()
+    An abstract base class for parser combinators (transformations
+    on parsers).
+    """
+
+    def __call__(self, *parsers: Tuple[Parser]) -> Parser:
+        raise NotImplementedError()
 
 
 # Parsers.
 
 class Success(Parser):
-	"""
-	Success(Parser)
-	
-	Makes any Parse a success.
-	"""
+    """
+    Success(Parser)
 
-        def __call__(parse: Parse) -> Parse:
-                return Parse(
-                        string = parse.string,
-                        result = parse.result,
-                        failed = False,
-                )
+    Makes any Parse a success.
+    """
+
+    def __call__(parse: Parse) -> Parse:
+        return Parse(
+                string = parse.string,
+                result = parse.result,
+                failed = False,
+        )
 
 
 class Failure(Parser):
-	"""
-	Failure(Parser)
-	
-	Makes any Parse a failure.
-	"""
+    """
+    Failure(Parser)
 
-        def __call__(parse: Parse) -> Parse:
-                return Parse(
-                        string = parse.string,
-                        result = parse.result,
-                        failed = True,
-                )
+    Makes any Parse a failure.
+    """
 
-	
+    def __call__(parse: Parse) -> Parse:
+        return Parse(
+                string = parse.string,
+                result = parse.result,
+                failed = True,
+        )
+
+
 class Nothing(Parser):
-	"""
-	Nothing(Parser)
-	
-	Parsers the the empty string (i.e. epsilon). Reduces to the 
-	identity function so that `Nothing(parse) = parse`.
-	"""
+    """
+    Nothing(Parser)
 
-        def __call__(parse: Parse) -> Parse:
-                return Parse(
-                        string = parse.string,
-                        result = parse.result,
-                        failed = parse.failed,
-                )
+    Parsers the the empty string (i.e. epsilon). Reduces to the
+    identity function so that `Nothing(parse) = parse`.
+    """
+
+    def __call__(parse: Parse) -> Parse:
+        return Parse(
+                string = parse.string,
+                result = parse.result,
+                failed = parse.failed,
+        )
 
 
 # Parser combinators.
 
 class All(ParserCombinator):
-	"""
-	All(ParserCombinator)
-	
-	Returns the concatenation of several parsers. If all of the 
-	parsers return a success, the concatenation returns a success
-	with their individual results enumerated into a list. If any of
-	the parsers return a failure, the concatenation does too.
-	"""
+    """
+    All(ParserCombinator)
 
-        def __call__(self, *parsers: Tuple[Parser]) -> Parser:
-                assert len(parsers)
+    Returns the concatenation of several parsers. If all of the
+    parsers return a success, the concatenation returns a success
+    with their individual results enumerated into a list. If any of
+    the parsers return a failure, the concatenation does too.
+    """
 
-                def parser(parse: Parse) -> Parse:
-                        result = tuple() # Stores enumerated results.
+    def __call__(self, *parsers: Tuple[Parser]) -> Parser:
+        assert len(parsers)
 
-                        for parser in parsers:
-                                parse = parser(parse)
+        def parser(parse: Parse) -> Parse:
+            result = tuple() # Stores enumerated results.
 
-                                if parse.failed:
-                                        return failure(parse)
+            for parser in parsers:
+                parse = parser(parse)
 
-                                result += (parse.result, )
+                if parse.failed:
+                    return failure(parse)
 
-                        return success(Parse(
-                                string = parse.string,
-                                result = result,
-                        )
+                result += (parse.result, )
 
-                return parser
+            return success(Parse(
+                    string = parse.string,
+                    result = result,
+            ))
+
+        return parser
 
 
 class Any(ParserCombinator):
-	"""
-	Any(ParserCombinator)
-	
-	Returns the alternation of several parsers. If any of the 
-	parsers return a success, the concatenation returns the first
-	successful parse. Otherwise it returns a failure.
-	"""
+    """
+    Any(ParserCombinator)
 
-        def __call__(self, *parsers: Tuple[Parser]) -> Parser:
-                assert len(parsers)
+    Returns the alternation of several parsers. If any of the
+    parsers return a success, the concatenation returns the first
+    successful parse. Otherwise it returns a failure.
+    """
 
-                def parser(parse: Parse) -> Parse:
+    def __call__(self, *parsers: Tuple[Parser]) -> Parser:
+        assert len(parsers)
 
-                        for parser in parsers:
-                                new_parse = parser(parse)
+        def parser(parse: Parse) -> Parse:
 
-                                if not new_perse.failed:
-                                        return success(new_parse)
+            for parser in parsers:
+                new_parse = parser(parse)
 
-                        return failure(parse)
+                if not new_perse.failed:
+                    return success(new_parse)
 
-				       
+            return failure(parse)
+
+
 class Maybe(ParserCombinator):
-	"""
-	Maybe(ParserCombinator)
-	
-	Returns the optional equivalent of a given parser. Reduces to
-	the alternation `Maybe(parse) = parse | Nothing`.
-	"""
+    """
+    Maybe(ParserCombinator)
 
-        def __call__(self, *parsers: Tuple[Parser]) -> Parser:
-                assert len(parsers) = 1
+    Returns the optional equivalent of a given parser. Reduces to
+    the alternation `Maybe(parse) = parse | Nothing`.
+    """
 
-                return Any(parser, Nothing)
+    def __call__(self, *parsers: Tuple[Parser]) -> Parser:
+        assert len(parsers) = 1
+
+        return Any(parser, Nothing)
