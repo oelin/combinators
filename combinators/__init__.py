8f37083e6aca31cd03a6c20539892e1d3d1b8b18
kale <42823429+oelin@users.noreply.github.com>
2023-03-19 15:08:54 +0000

    Update __init__.py

--- combinators/__init__.py
+++ combinators/__init__.py
@@ -1,8 +1,28 @@
-from . combinators import Parse, Parser, And, Or, Maybe, Lambda
-from . parsers import Match
+from dataclasses import dataclass
 
 
-# Operators.
+last = lambda x: x[-1]
+init = lambda x: x[: -1]
 
-Parser.__and__ = And
-Parser.__or__ = Or
+
+@dataclass
+class parser:
+        f: callable = None
+        __call__ = lambda self, x: self.f(x)
+        __or__ = lambda f, g: parser(lambda x: f(x) or g(x))
+        __add__ = lambda f, g: parser(lambda x: (x := p(x)) and (y := q(x)) and (init(x) + y))
+        __neg__= lambda f: parser(lambda x: (y := f(x)) and (*init(x), last(y)))
+        __gt__ = lambda f, g: parser(lambda x: (x := f(x)) and (g(*init(x)), last(x)))
+
+
+import re
+
+def match(token):
+
+        def f(x):
+                string = last(x)
+                lexeme = re.match(token, string)
+
+                if lexeme:
+                        return lexeme.group(), string[lexeme.end() :]
+        return parser(f)
